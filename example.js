9d757e9d22de371bf24f6c5f396b3f8450d863c2
iamnotej <90478926+iamnotej@users.noreply.github.com>
2022-01-14 00:18:43 +0100

    did all the steps

--- example.js
+++ example.js
@@ -1,2 +1,12 @@
 const showAlert = () => 
     alert('yes!! i did my first alert message :)');
+    
+
+
+
+const showContact = () => 
+    alert('we are still working for that for info contact Jeton!');
+
+
+
+
